6ad9b3007e71db0376309f1f7929b040a6cf5d52
Elio Zevallos <elio.zevallos@es.gfi.world>
2019-07-19 12:01:40 +0200

    finished getGreeting function test

--- test/content-utils.test.js
+++ test/content-utils.test.js
@@ -2,10 +2,19 @@ import chai from 'chai';
 import utils from '../app/src/content-utils.js'
 
 describe('getGreeting', function() {
-  it('returns the right greeting', function() {
-    
+  it('a name provided returns "hello <name>"', function() {
     let response = utils.getGreeting('elio')
-
     chai.assert.equal(response, 'hello elio');
   });
+
+  it('an empty string provided returns "hello anonymous"', function() {
+    let response = utils.getGreeting('');
+    chai.expect(response).to.equal('hello anonymous')
+  })
+
+  it('a null value provided returns "hello anonymous"', function() {
+    const should = chai.should(); //should interface extends Object.prototype
+    let response = utils.getGreeting(null);
+    response.should.equal('hello anonymous');
+  })
 });
